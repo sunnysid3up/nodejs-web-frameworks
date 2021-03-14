8921ca9a3f41e9396b3580bee024e34d4693a957
Sunny Kim <sungunkim367@gmail.com>
2021-03-14 20:14:49 +0900

    [ADD] First commit]

---
+++ README.md
@@ -0,0 +1,19 @@
+# Popular Node.js Web Frameworks 
+A list of popular Node.js web frameworks ranked by the number of GitHub stars, automatically updated every week.
+
+Last update: 2021-03-14T11:13:28.049Z
+
+| Name          | Description          | Stars                     | Forks          | Issues               | First Commit        | Last Commit         | Language          |
+|---------------|----------------------|---------------------------|----------------|----------------------|---------------------|---------------------|-------------------|
+| [express](https://github.com/expressjs/express) | Fast, unopinionated, minimalist web framework for node. | 52277 | 8772 | 163 | 2009 | 2021-03-14 | JS |
+| [meteor](https://github.com/meteor/meteor) | Meteor, the JavaScript App Platform | 42336 | 5154 | 134 | 2012 | 2021-03-13 | JS |
+| [nest](https://github.com/nestjs/nest) | A progressive Node.js framework for building efficient, scalable, and enterprise-grade server-side applications on top of TypeScript & JavaScript (ES6, ES7, ES8) 🚀 | 35301 | 3324 | 58 | 2017 | 2021-03-14 | TS |
+| [koa](https://github.com/koajs/koa) | Expressive middleware for node.js using ES2017 async functions | 30858 | 2958 | 40 | 2013 | 2021-03-14 | JS |
+| [sails](https://github.com/balderdashy/sails) | Realtime MVC Framework for Node.js | 21861 | 1911 | 463 | 2012 | 2021-03-13 | JS |
+| [loopback](https://github.com/strongloop/loopback) | LoopBack makes it easy to build modern applications that require complex integrations. | 13284 | 1233 | 2 | 2013 | 2021-03-13 | JS |
+| [feathers](https://github.com/feathersjs/feathers) | A framework for real-time applications and REST APIs with JavaScript and TypeScript | 13229 | 619 | 120 | 2011 | 2021-03-12 | TS |
+| [hapi](https://github.com/hapijs/hapi) | The Simple, Secure Framework Developers Trust | 13122 | 1322 | 22 | 2011 | 2021-03-14 | JS |
+
+## Contribute 
+
+Create an issue or pull request if you would like to add more frameworks :)
---
+++ main.js
@@ -0,0 +1,81 @@
+const fsp = require('fs').promises;
+const https = require('https')
+const repos = require('./repos.json')
+
+async function main() {
+  const result = []
+  for (const repo of repos) {
+    const repoData = await request(repo)
+    result.push(new Repo(repoData))
+  }
+  await write(rankByStars(result))
+}
+
+main().then(_ => console.log('Done.'))
+
+
+// Helper functions
+
+function request(repo) {
+  const options = {
+    hostname: 'api.github.com',
+    path: '/repos/' + repo.path,
+    method: 'GET',
+    headers: {
+      'Authorization': `token ${process.env.GTOKEN}`,
+      'Content-Type': 'application/json',
+      'user-agent': 'node.js',
+    }
+  }
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      res.setEncoding('utf8')
+      let resBody = ''
+      res.on('data', (chunk) => {
+        resBody += chunk
+      })
+      res.on('end', () => {
+        resolve(JSON.parse(resBody))
+      })
+    })
+    req.on('error', (err) => {
+      console.log(err.name)
+      console.log(err.message)
+      reject(err)
+    })
+    req.end();
+  })
+}
+
+function rankByStars(repos) {
+  return repos.sort((a, b) => a.stargazers_count - b.stargazers_count).reverse()
+}
+
+async function write(sortedRepos) {
+  let data = `# Top Node.js Web Frameworks \nA list of popular Node.js web frameworks ranked by the number of GitHub stars, automatically updated every week.\n\nLast update: ${new Date().toISOString()}
+
+| Name          | Description          | Stars                     | Forks          | Issues               | First Commit        | Last Commit         | Language          |
+|---------------|----------------------|---------------------------|----------------|----------------------|---------------------|---------------------|-------------------|`
+  for (const repo of sortedRepos) {
+    const language = repo.language === "TypeScript" ? "TS" : "JS"
+    data += `\n| [${repo.name}](${repo.html_url}) | ${repo.description} | ${repo.stargazers_count} | ${repo.forks} | ${repo.open_issues} | ${repo.created_at} | ${repo.updated_at} | ${language} |`
+  }
+  data += `\n\n## Contribute \n\nCreate an issue or pull request if you would like to add more frameworks :)`
+  await fsp.writeFile('README.md', data)
+  console.log("Updated README.")
+}
+
+
+// Struct
+
+function Repo(data) {
+  this.name = data.name
+  this.description = data.description
+  this.html_url = data.html_url
+  this.stargazers_count = data.stargazers_count
+  this.forks = data.forks
+  this.open_issues = data.open_issues
+  this.language = data.language
+  this.created_at = new Date(data.created_at).getFullYear()
+  this.updated_at = data.updated_at.split("T")[0]
+}
